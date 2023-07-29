7274d29122d1860801a3c5242b35c78f61982507
Noah Velasco <noahvelasco299@gmail.com>
2023-07-28 21:46:51 -0500

    modularized the list item component

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
-import CurrentWeather from './src/components/CurrentWeather'
+import CurrentWeather from './src/screens/CurrentWeather'
+import City from './src/screens/City'
+import UpcomingWeather from './src/screens/UpcomingWeather'
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <CurrentWeather />
+      <City />
     </View>
   )
 }
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'royalblue',
   },
-}
+})
 export default App
---
+++ src/screens/UpcomingWeather.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  StatusBar,
+  ImageBackground,
+} from 'react-native'
+
+import ListItem from '../components/ListItem'
+const DATA = [
+  {
+    dt_text: '2023-02-18 12:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [{ main: 'Clear' }],
+  },
+  {
+    dt_text: '2023-02-18 15:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [{ main: 'Clear' }],
+  },
+  {
+    dt_text: '2023-02-18 18:00:00',
+    main: {
+      temp_max: 8.55,
+      temp_min: 7.55,
+    },
+    weather: [{ main: 'Rain' }],
+  },
+]
+const Empty = () => (
+  <View>
+    <Text>Empty</Text>
+  </View>
+)
+
+const UpcomingWeather = () => {
+  const renderItem = ({ item }) => (
+    <ListItem
+      dt_text={item.dt_text}
+      min={item.main.temp_min}
+      max={item.main.temp_max}
+      condition={item.weather[0].main}
+    />
+  )
+  const { container, image } = styles
+  return (
+    <SafeAreaView style={container}>
+      <ImageBackground
+        source={require('../../assets/clouds.jpg')}
+        style={image}
+      >
+        <Text>Upcoming weather</Text>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.dt_text}
+          ItemSeparatorComponent={() => (
+            <View style={{ backgroundColor: '#CCCCCC', height: 2 }} />
+          )}
+          ListEmptyComponent={Empty}
+        />
+      </ImageBackground>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+
+  image: {
+    flex: 1,
+  },
+})
+export default UpcomingWeather
